809100ae3c8d8f9b6519471896d4bda56dea7c0b
Coderhypo <i@ihypo.net>
2015-09-23 14:44:11 +0800

    ex2

---
+++ hypo/week1/ex2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+#A comment,this is so you can read your program later.
+#Anything after the # is ignored by python.
+
+print "I could hava code like this." # and the comment after is ignored
+
+#You can also use a comment to "disable" or comment out a piece of code:
+#print "This won't run"
+
+print "This will run."
+
+#我日了狗加分题目到底要检查什么错误_(:з」∠)_
+
