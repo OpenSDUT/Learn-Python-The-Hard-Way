52805615bf40d41d8bd000710511972e90531c81
Coderhypo <i@ihypo.net>
2015-09-23 15:10:45 +0800

    ex3 and ex3 plus

---
+++ hypo/week1/ex3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6 #30
+print "Roosters", 100 - 25 * 3 % 4 #97
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 2 / 4 + 6 #7
+
+print "Is it true that 3 + 2 < 5 - 7?" #False
+
+print 3 + 2 < 5 - 7 #False
+
+print "What is 3 + 2?",3 + 2 #5
+print "What is 5 - 7?",5 - 7 #-2
+
+print "Oh,that's why is's False"
+
+print "How about some more."
+
+print "Is it greater?",5 > -2 #True 
+print "Is it greater or equal?",5 >= -2 #True
+print "Is it less or equal?",5 <= -1 #False
+
