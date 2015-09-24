900198651143732942600f4605bcecbea01caf51
Coderhypo <i@ihypo.net>
2015-09-24 23:47:00 +0800

    ex10

---
+++ hypo/week1/ex10.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+tabby_cat = "\tT'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip \n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+print "%r %s" % (""""Hello",he said. """,""""Hello",he said.""")
