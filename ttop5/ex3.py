21990345a00c8c738628ee9f165814761a20adec
ttop5 <ttop5@qq.com>
2015-09-26 22:29:39 +0800

    First week

---
+++ ttop5/ex3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+print "I will now acount my chickens."
+
+print "Hens", 25 + 30 / 6
+print "roosters", 100 - 25 * 3 % 4
+
+print "Now I will acount the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False!"
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
