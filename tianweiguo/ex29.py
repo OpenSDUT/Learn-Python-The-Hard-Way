3e2f19347b999ef79f1306972a4f27f1a9e9b508
tianweiguo <17865933200@163.com>
2015-10-02 14:20:20 +0800

    third week

---
+++ tianweiguo/ex29.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+people=20
+cats=30
+dogs=15
+if people<cats:
+    print "Too many cats! The world is doomed"
+
+if people>cats:
+    print "Not many cats! The world is saved."
+
+if people<dogs:
+    print "The world is drooed on."
+
+if people>dogs:
+    print "The world is dry."
+dogs+=5
+if people>=dogs:
+    print "People are greater than or equal dogs."
+if people<=dogs:
+    print "People are less than or equal dogs."
+if people==dogs:
+    print "People are dogs."
---
+++ tianweiguo/ex34.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+animals=['bear','python','peacock','kangaroo','whale','platypus']
+print animals
+print "The animal at 1"
+print animals[0]
+print "The 3rd animal ."
+print animals[2]
+print "The 5th animal."
+print animals[4]
