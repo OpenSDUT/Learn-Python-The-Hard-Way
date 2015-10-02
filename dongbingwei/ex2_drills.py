e14857d84d424e234c30e1dcec8538f8fa221d74
ruitian <chinabingwei@163.com>
2015-10-02 16:13:23 +0800

    study drills

---
+++ dongbingwei/ex2_drills.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Python still ignores that as code, but it's used as a
+# kind of "hack" or workaround for problems with setting
+# and detecting the format of a file. You also find a similar
+# kind of comment for editor settings.
+
+# study drill 1
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
