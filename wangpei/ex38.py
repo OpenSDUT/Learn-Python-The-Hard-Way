c4511252359bbb743d9f1c9e86a0d0bf80d0fd5c
Timeship <leonwowo@gmail.com>
2015-10-08 22:16:51 +0800

    5th

--- wangpei/ex38.py
+++ wangpei/ex38.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 # coding=utf-8
+ten_things ="Apples Oranges Crows Telephone Light Sugar"
+print("Wait there are not 10 things in that list.let's fix that")
 
+stuff = ten_things.split(' ')
+more_stuff = ['Day','Night','Song','Frisbee','Corn','Banana','Girl','Boy']
+
+while len(stuff)!=10:
+    next_one = more_stuff.pop()
+    print("Adding:",next_one)
+    stuff.append(next_one)
+    print("There are %d items now"%len(stuff))
+print("There we go:",stuff)
+print("let's do some things with stuff")
+print(stuff[1])
+print(stuff[-1])
+print(stuff.pop())
+print(' '.join(stuff))
+print('#'.join(stuff[3:5]))
