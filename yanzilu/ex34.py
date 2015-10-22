b45f5becdc5b5cf8e40a8a283b110bcf819b05f8
CodeBall <m@yanzl.net>
2015-10-22 19:29:42 +0800

    Exercise thirty-four

---
+++ yanzilu/ex34.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#Exercise 34:Accessing Elements of Lists
+
+animals = ['bear','python','peacock','kangarro','whale','platypus']
+
+print "The animal at 1 is :",animals[1]
+print "The third(3rd) animal is:",animals[2]
+print "The first(1st) animal is :",animals[0]
+print "The animal at 3:",animals[3]
+print "The fifth(5th) animal is :",animals[4]
+print "The animal at 2:",animals[2]
+print "The sixth(6th) animal is :",animals[5]
+print "The animal at 4 is :",animals[4]
